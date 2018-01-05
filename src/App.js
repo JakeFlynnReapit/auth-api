0695976170b5bc2586872238a3b8ed6ea5bf519e
Jake <jflynn@reapit.com>
2018-01-05 14:46:59 +0000

    Added: Dummy data and counters
    Implemented some false data for testing purposes, displayed counters for the total time the entire playlists amount to and the amount of playlists there are

--- src/App.js
+++ src/App.js
@@ -4,12 +4,120 @@ import './App.css';
 let defaultStyle = {
   color: '#fff',
 }
+let dummyData = {
+  user: {
+    name: 'Jake',
+    playlists: [
+      {
+        name: 'Favourites',
+        songs: [
+          {
+            name: 'I Fall Apart',
+            duration: 208000
+          },
+          {
+            name: 'Rapper',
+            duration: 143000
+          },
+          {
+            name: 'I\'m Not Racist',
+            duration: 415000
+          },
+          {
+            name: 'Boss',
+            duration: 106000
+          }
+        ]
+      },
+      {
+        name: 'House',
+        songs: [
+          {
+            name: 'Darkness',
+            duration: 182000
+          },
+          {
+            name: 'Say To Me',
+            duration: 256000
+          },
+          {
+            name: 'Gecko (Overdrive)',
+            duration: 166000
+          },
+          {
+            name: 'Last All Night (Koala)',
+            duration: 245000
+          }
+        ]
+      },
+      {
+        name: 'Metal',
+        songs: [
+          {
+            name: 'Before I Forget',
+            duration: 243000
+          },
+          {
+            name: 'Naysayer',
+            duration: 210000
+          },
+          {
+            name: 'Gravity',
+            duration: 201000
+          },
+          {
+            name: 'Downfall',
+            duration: 245000
+          }
+        ]
+      },
+      {
+        name: 'Lit',
+        songs: [
+          {
+            name: 'Gucci Gang',
+            duration: 131000
+          },
+          {
+            name: 'Look At Me',
+            duration: 152000
+          },
+          {
+            name: 'Little Boy',
+            duration: 25100
+          },
+          {
+            name: 'The Plan',
+            duration: 260000
+          }
+        ]
+      }
+    ]
+  }
+};
 
-class Aggregate extends Component {
+class PlaylistCounter extends Component {
   render() {
     return (
       <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
-        <h2>Number Text</h2>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    },[]);
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration
+    }, 0);
+    return (
+      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+        <h2>{Math.round(totalDuration * 2.7778E-7) > 1 ? Math.round(totalDuration * 2.7778E-7) + ' hours' 
+          : Math.round(totalDuration * 2.7778E-7) + ' hour'}</h2>
       </div>
     );
   }
@@ -45,17 +153,33 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    var that = this;
+    setTimeout(function() {
+      that.setState({serverData: dummyData});
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
-        <h1 style={{...defaultStyle, 'font-size': '54px',}}>Title</h1>
-        <Aggregate/>
-        <Aggregate/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
+        {this.state.serverData.user ?
+        <div>
+          <h1 style={{...defaultStyle, 'font-size': '54px',}}> 
+            {this.state.serverData.user.name}'s Playlists
+          </h1>
+          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
+          <HoursCounter playlists={this.state.serverData.user.playlists}/>
+          <Filter/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+        </div> : <h1 style={{...defaultStyle}}>Loading...</h1>
+        }
       </div>
     );
   }
