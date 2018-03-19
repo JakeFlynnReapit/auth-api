c53d59980a4f1388da8a9b82593e7a6742bddc79
Jake <jflynn@reapit.com>
2018-03-19 10:27:55 +0000

    coolio spinny thing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 class Landing extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			button: {
+				className: 'button'
+			}
+		}
+	}
 	render() {
 		return (
 			<div className="landing">
 				<div>
 					<h1>Playlister</h1>
 					<h3>All of the music you love.</h3>
-					<div className="button" onClick={() => {
+					<div className={this.state.button.className} onClick={() => {
+							this.setState({
+								button:{className:'sending button'}
+							})
 							if (window.location.href.includes('localhost')) {
 								window.location = 'http://localhost:8888/login'
 							} else {
